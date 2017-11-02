86faaac27769e5e18c0cf98627eacd05cc08c902
Rohit Negi <rohit.negi@elucidata.io>
2017-11-02 18:53:32 +0530

    1. added the functions that the package will perform in first release

---
+++ __init__.py
@@ -0,0 +1,1 @@
+print "chal gya"
---
+++ operations.py
@@ -0,0 +1,24 @@
+
+def create_package(args):
+    '''
+    It will create the lambda boiler plate
+    '''
+    print args
+
+def test_package(args):
+    '''
+    It will test the lambda boiler plate with the code
+    '''
+    pass
+
+def deploy_package(args):
+    '''
+    It will deploy the lambda to s3
+    '''
+    pass
+
+def build_package(args):
+    '''
+    It will build the .zip package after reducing the size
+    '''
+    pass
