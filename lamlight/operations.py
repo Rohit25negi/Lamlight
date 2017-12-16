7b993169f6023ba38416b92ee6e391bf1e63da37
rohit negi <rohit.negi@elucidata.io>
2017-12-16 12:07:36 +0530

    1. corrected the zipping code

--- lamlight/operations.py
+++ lamlight/operations.py
@@ -132,7 +132,7 @@ def build_package():
     command_list.append((hlpr.remove_trees, ('temp_dependencies/',)))
     hlpr.run_dependent_commands(command_list)
     try:
-        os.system('cd temp_dependencies && zip -r ../.requirements.zip -i $PWD')
+        os.system('cd temp_dependencies && zip -r ../.requirements.zip $PWD')
         shutil.rmtree('temp_dependencies/')
         cwd = os.path.basename(os.getcwd())
         zip_path = "/tmp/{}".format(cwd)
