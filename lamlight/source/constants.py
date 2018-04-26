4b835e4858fe6674767d446cb0f0af0769484b00
rohit negi <rohit.negi@elucidata.io>
2018-04-26 21:04:07 +0530

    1. added the documentation 2. put the module documentation

--- lamlight/source/constants.py
+++ lamlight/source/constants.py
@@ -1,3 +1,8 @@
+'''
+Module constains the constants which is used by the AWS lambda handler module
+'service_router'
+
+'''
 RESOURCE_QUERY_DATA = 'query_param'
 RESOURCE_PAYLOAD_DATA = 'body_payload'
 DEPENDENCIES_PATH = '/tmp/dependencies/'
--- lamlight/source/db_models/db_creds.py
+++ lamlight/source/db_models/db_creds.py
@@ -1,4 +1,11 @@
+'''
+Module contains the database credentials if in case AWS lambda function needs to
+connect to database. It takes the database credentials from the environment
+variable.
+'''
+
 import os
+
 
 DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_PORT = os.getenv('DATABASE_PORT')
