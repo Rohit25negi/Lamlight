528e4ae796f2332dcf2218cc1597c96342245f49
Rohit Negi <rohit.negi@elucidata.io>
2018-05-12 23:52:53 +0530

    1. added the documentation 2. pep8 p

--- lamlight/source/resources/example_resource.py
+++ lamlight/source/resources/example_resource.py
@@ -2,17 +2,40 @@ from resources import Resource
 
 
 class ExampleResource(Resource):
-
+    """
+    This is the resource class which handles the HTTP request on /example-resource
+    endpoint
+    """
     def __init__(self,query_param,body_payload):
+        """
+        Constructor
+
+        Parameters
+        -----------
+        query_param: dict
+            dictionary containing the query parameter sent with the request
         
+        body_payload: dict
+            dictionary containing the body payload sent with the request
+
+        """
         self.query_params = query_param
         self.body_payload =  body_payload
         
     def get(self):
+        """
+        Method handles the get request on /example-resource
+        """
         return 'Hello World'
 
     def post(self):
+        """
+        Method handles the post request on /example-resource
+        """
         pass
 
     def patch(self):
+        """
+        Method handles the patch request on /example-resource
+        """
         pass
